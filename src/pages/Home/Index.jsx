0df720767da0464d349127978ab9c3f08c2ce894
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-06 14:53:21 -0300

    estilizando componentes

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -2,11 +2,11 @@ import './style.css'
 
 export function Home() {  
   return (
-    <>
+    < div className="container">
       <h1>Lista de Presenca</h1>
       <input type="text" placeholder="Digite um nome: "/>
       <button type="button">Adcionar</button>
-    </>
+    </div>
   )
 }
 
